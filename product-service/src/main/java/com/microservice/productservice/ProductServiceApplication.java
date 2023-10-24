746bd3d5e34f0b7c2969ddf85244fe872ceec83a
Durai <duraisingan2001p@gmail.com>
2023-10-24 17:18:44 +0530

    some text informatio added

--- product-service/src/main/java/com/microservice/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservice/productservice/ProductServiceApplication.java
@@ -3,6 +3,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class ProductServiceApplication {
@@ -10,3 +14,5 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 }
+
+
--- product-service/src/test/java/com/microservice/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/microservice/productservice/ProductServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.microservice.productservice;
 
 import com.microservice.productservice.dto.ProductRequest;
 import com.microservice.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,16 +46,21 @@ class ProductServiceApplicationTests {
 		mongoDBContainer.start();
 	}
 
-//	@Test
-//	void shouldCreateProduct() throws Exception {
-//		ProductRequest productRequest = getProductRequest();
-//		String productRequestString = objectMapper.writeValueAsString(productRequest);
-//		mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
-//						.contentType(MediaType.APPLICATION_JSON)
-//						.content(productRequestString))
-//				.andExpect(status().isCreated());
-//		Assertions.assertEquals(1, productRepository.findAll().size());
-//	}
+	@AfterAll
+	static void stopContainers() {
+		mongoDBContainer.stop();
+	}
+
+	@Test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(productRequestString))
+				.andExpect(status().isCreated());
+		Assertions.assertEquals(1, productRepository.findAll().size());
+	}
 
 	private ProductRequest getProductRequest() {
 		return ProductRequest.builder()
@@ -65,7 +71,26 @@ class ProductServiceApplicationTests {
 	}
 }
 
+
 //	    Testcontainers is an open-source Java library that provides lightweight, throwaway instances of common databases,
 //		message brokers, or other external services that your Spring Boot application might depend on.
 //		It's often used in testing scenarios to create and manage these services as Docker containers, making it easier
 //		to write integration tests for your Spring
+
+
+//Authorisation between two services is known as Oauth ex authorisation between google photos and gallery
+
+//why it is introduced means we cannot trust any one and give our user name and password to any third party application
+//whom we can trust google if we are using google drive
+
+//when we try to sign in to any gallery it will redirect ourself and get authorised on google so in that link if we sign
+//in means google will list out which permission it has wanted if we acknowled it it will get the access
+
+
+//Rest api the key thing is state is managed at the client side itslef it makes rest api more scalable.
+
+//it does not need to remember the client state
+
+//every request is the new request to the rest api i.e is does not depend on previous request and responses
+
+//state is maintained at the clinet side itslef
